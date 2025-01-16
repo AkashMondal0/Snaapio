41f0911463462cd0ffd0fcfd57a4a14da57b950c
Akash Mondal <akash2003mondal@gmail.com>
2025-01-16 23:16:18 +0530

    Refactor theme handling by extracting SwitchDarkComponent and optimizing theme initialization logic

--- app/setting/theme.tsx
+++ app/setting/theme.tsx
@@ -1,16 +1,15 @@
-import { memo, useCallback, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import { TouchableOpacity, View } from "react-native";
 import { localStorage } from "@/lib/LocalStorage";
 import AppHeader from "@/components/AppHeader";
-import { ThemedView, useTheme, themeColors, Switch } from "hyper-native-ui";
 import { Icon } from "@/components/skysolo-ui";
 import { Text } from "hyper-native-ui";
 import { configs } from "@/configs";
+import { ThemedView, useTheme, themeColors, Switch } from "hyper-native-ui";
 
 
 const ThemeSettingScreen = memo(function HomeScreen({ navigation }: any) {
     const { changeTheme, toggleTheme, themeScheme } = useTheme();
-    const [state, setState] = useState(themeScheme === "dark" ? true : false);
 
     const handleChange = useCallback(async (themeName: any) => {
         try {
@@ -20,19 +19,6 @@ const ThemeSettingScreen = memo(function HomeScreen({ navigation }: any) {
             console.error("Error in setting theme", error);
         }
     }, [])
-
-    const onValueChange = useCallback(async (value: boolean) => {
-        try {
-            const condition = value ? "dark" : "light"
-            if (themeScheme === condition) return;
-            await localStorage("set", configs.themeSchema, condition);
-            toggleTheme();
-        } catch (error) {
-            console.error("Error in setting theme", error);
-        }
-        setState(value)
-        toggleTheme()
-    }, [themeScheme])
 
     return (
         <ThemedView style={{
@@ -63,31 +49,52 @@ const ThemeSettingScreen = memo(function HomeScreen({ navigation }: any) {
                     </TouchableOpacity>
                 })}
             </View>
-            <View style={{
-                width: "100%",
-                height: 75,
-                padding: 15,
-                paddingHorizontal: 20,
-                display: 'flex',
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-            }}>
-                <View
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        gap: 14,
-                        alignItems: "center"
-                    }}>
-                    <Icon iconName={"Moon"} size={36} />
-                    <Text style={{ fontWeight: "600" }} variant="H6">
-                        Switch Dark
-                    </Text>
-                </View>
-                <Switch onValueChange={onValueChange} isChecked={state} size="medium" />
-            </View>
+            <SwitchDarkComponent />
         </ThemedView>
     )
 })
 export default ThemeSettingScreen;
+
+
+const SwitchDarkComponent = () => {
+    const { toggleTheme, themeScheme } = useTheme();
+    const [state, setState] = useState(themeScheme === "dark");
+
+    const onValueChange = useCallback(async (value: boolean) => {
+        try {
+            const condition = value ? "dark" : "light"
+            if (themeScheme === condition) return;
+            await localStorage("set", configs.themeSchema, condition);
+            toggleTheme();
+        } catch (error) {
+            console.error("Error in setting theme", error);
+        } finally {
+            setState(value)
+        }
+    }, [themeScheme]);
+
+    return (<View style={{
+        width: "100%",
+        height: 75,
+        padding: 15,
+        paddingHorizontal: 20,
+        display: 'flex',
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    }}>
+        <View
+            style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: 14,
+                alignItems: "center"
+            }}>
+            <Icon iconName={"Moon"} size={36} />
+            <Text style={{ fontWeight: "600" }} variant="H6">
+                Switch Dark
+            </Text>
+        </View>
+        <Switch onValueChange={onValueChange} isChecked={state} size="medium" />
+    </View>)
+}
--- provider/PreConfiguration.tsx
+++ provider/PreConfiguration.tsx
@@ -1,22 +1,19 @@
-import React, { useCallback } from "react";
-import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/redux-stores/store";
-import { Appearance } from 'react-native';
+import { Session } from '@/types';
+import { configs } from '@/configs';
+import { useDispatch } from "react-redux";
+import React, { useCallback } from "react";
 import { localStorage } from '@/lib/LocalStorage';
 import { getSecureStorage } from '@/lib/SecureStore';
+import * as SplashScreen from 'expo-splash-screen';
 import { setSession } from '@/redux-stores/slice/auth';
-import { Session } from '@/types';
+import { useTheme, StatusBar } from "hyper-native-ui";
 import { fetchUnreadNotificationCountApi } from '@/redux-stores/slice/notification/api.service';
-import { configs } from '@/configs';
-import { useTheme, StatusBar, ThemeSchemaType } from "hyper-native-ui";
-
+let loaded = false;
 
 const PreConfiguration = () => {
     const dispatch = useDispatch();
-    const { setInitialTheme, toggleTheme, themeScheme } = useTheme();
-    const loaded = useSelector((state: RootState) => state.AuthState.loaded, (pre, next) => pre === next);
+    const { setInitialTheme } = useTheme();
 
     const delayFunction = useCallback(async () => {
         await new Promise((resolve) => setTimeout(resolve, 300));
@@ -25,12 +22,13 @@ const PreConfiguration = () => {
 
     // initialize theme value
     const initialize = useCallback(async () => {
-        if (loaded) return
+        if (loaded) return;
+        loaded = true;
         const p1 = localStorage("get", configs.themeSchema) as any;
         const p2 = localStorage("get", configs.themeName) as any;
         const p3 = getSecureStorage<Session["user"]>(configs.sessionName);
         const data = await Promise.all([p1, p2, p3])
-        
+
         if (data[0] && data[1]) {
             setInitialTheme({ themeSchema: data[0], themeName: data[1] });
         }
@@ -40,26 +38,11 @@ const PreConfiguration = () => {
             dispatch(fetchUnreadNotificationCountApi() as any);
         }
         return;
-    }, [loaded])
-
-    const onValueChange = useCallback(async (value: ThemeSchemaType) => {
-        try {
-            if (themeScheme === value) return;
-            await localStorage("set", configs.themeSchema, value);
-            toggleTheme();
-        } catch (error) {
-            console.error("Error in setting theme", error);
-        }
-    }, [themeScheme])
+    }, [])
 
     useEffect(() => {
         initialize();
-        const unSubscribe = Appearance.addChangeListener(({ colorScheme }) => {
-            onValueChange(colorScheme as ThemeSchemaType)
-        })
-
-        return () => { unSubscribe.remove(); }
-    }, [themeScheme])
+    }, [])
 
     return (<StatusBar />)
 }
